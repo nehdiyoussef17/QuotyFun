33bf05a9746863f87c43c50fd5d8550def55729e
Nehdi youssef <youssef.nehdi@esprit.tn>
2022-02-09 11:29:08 +0100

    Initial Commit

---
+++ QuotyFun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuotyFun
+//
+//  Created by odc on 9/2/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuotyFun/QuotyFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotyFunApp.swift
+//  QuotyFun
+//
+//  Created by odc on 9/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuotyFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
